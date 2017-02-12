e3288d782358fb49838fae0fbb6dd2e2222eca0c
Sagothon <karol.piszczek@gmail.com>
2017-02-12 20:34:30 +0100

    improves

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -16,30 +16,34 @@ def ParallelSSH(ip_table, other_config, command):
 
     client = ParallelSSHClient(lista, user='', password='', port=22, num_retries=1)
     output = client.run_command('mca-status', stop_on_errors=False)
-
-    device = {'dev_name':'', 'firmware':'', 'wireless_mode':'', 'signal':0, 'ccq':0, 'air_q':0, 'air_c':0, 'freq':0, 'uptime':0, 'product':''}
-
     for node in output:
-        #print(output[node]['exception'])
-        for line in output[node]['stdout']:
-            if 'WlanOpmode' in line:
-                device['wireless_mode'] = line.split('=')[1]
-            if 'signal' in line:
-                device['signal'] = int(line.split('-')[1])
-            if 'ccq' in line:
-                device['ccq'] = int(float(line.split('=')[1])/10)
+        device = {'dev_name':'', 'firmware':'', 'wireless_mode':'', 'signal':0, 'ccq':0, 'air_q':0, 'air_c':0, 'freq':0, 'uptime':0, 'product':'', 'exception':''}
+           
+        if str(output[node]['exception']) == 'None':
+            device['exception'] = 'OK'
+            for line in output[node]['stdout']:
+                if 'wlanOpmode' in line:
+                    device['wireless_mode'] = line.split('=')[1]
+                if 'signal' in line:
+                    device['signal'] = int(line.split('-')[1])
+                if 'ccq' in line:
+                    device['ccq'] = int(float(line.split('=')[1])/10)
+                if 'uptime' in line:
+                    device['uptime'] = int(line.split('=')[1])/86400
 
-            if 'deviceName' in line:
-                line2 = line.split(',')
-                for i in line2:
-                    if 'Name' in i:
-                        device['dev_name'] = i.split('=')[1]
-                    if 'firmware' in i:
-                        device['firmware'] = i.split('=')[1]
-                    if 'platform' in i:
-                        device['product'] = i.split('=')[1]
+                if 'deviceName' in line:
+                    line2 = line.split(',')
+                    for i in line2:
+                        if 'Name' in i:
+                            device['dev_name'] = i.split('=')[1]
+                        if 'firmware' in i:
+                            device['firmware'] = i.split('=')[1]
+                        if 'platform' in i:
+                            device['product'] = i.split('=')[1]
+            c.execute("UPDATE monitoring_device SET dev_name=?, firmware=?, product=?, wireless_mode=?, signal=?, ccq=?, uptime=?, error=? WHERE ip=?", (device['dev_name'], device['firmware'], device['product'], device['wireless_mode'], device['signal'], device['ccq'], device['uptime'], device['exception'], node))     
             
-        c.execute("UPDATE monitoring_device SET dev_name=?, firmware=?, product=?  WHERE ip=?", (device['dev_name'], device['firmware'], device['product'], node))     
-        data_base.commit()
-
+        else:
+            device['exception'] = 'failure'
+            c.execute("UPDATE monitoring_device SET error=? WHERE ip=?", (device['exception'], node))     
+    data_base.commit()
 ParallelSSH('dfd', 4, 7)
