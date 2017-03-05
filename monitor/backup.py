5ac63aa0dd23ff0e6de1ea188dae821ecb13dddd
Sagothon <karol.piszczek@gmail.com>
2017-03-05 16:24:35 +0100

    done backup script

--- monitor/backup.py
+++ monitor/backup.py
@@ -1,18 +1,26 @@
-from pssh.pssh_client import ParallelSSHClient
-from pssh.exceptions import AuthenticationException, \
-  UnknownHostException, ConnectionErrorException
-#import sqlite3
+import subprocess
+import sqlite3
 
-#data_base = sqlite3.connect('../stronka/db.sqlite3')
-#c = data_base.cursor()
-#c.execute('SELECT ip, login, password, port FROM monitoring_device')
-#config = c.fetchall()
+data_base = sqlite3.connect('../stronka/db.sqlite3')
+c = data_base.cursor()
+c.execute('SELECT ip, login, password, port FROM monitoring_device')
+config = c.fetchall()
 
-#host_configurations = {}
-#print("fdfgd")
-#for ip in config:
-#    host_configurations[ip[0]] = {'user': ip[1], 'password': ip[2], 'port': 9922}
+host_configurations = {}
+print(config)
 
-client = ParallelSSHClient(hosts=")
-client.copy_file(local_file="~/programy/monitoring/backup/siema", remote_file="/tmp/systemm.cfg")
-client.copy_remote_file
+for ip in config:
+    host_configurations[ip[0]] = {'user': ip[1], 'password': ip[2], 'port': 9922}
+    ip_add = ip[0]
+    user = ip[1]
+    password = "\"" + ip[2] + "\""
+    port = "9922"
+
+    command = "sshpass -p %s scp -P %s -o StrictHostKeyChecking=no root@%s:/tmp/system.cfg /home/karol/programy/monitoring/back/%s" %(password, port, ip_add, ip_add)
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    (output, err) = p.communicate()
+
+
+#command = "sshpass -p \"Kotka09\" scp -P 9922 root@192.168.5.100:/tmp/system.cfg /home/karol/programy/monitoring/back/conf"
+
+#
