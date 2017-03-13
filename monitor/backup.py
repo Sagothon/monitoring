0e84c47f06688e223ae7b383cd223c9327de374c
Sagothon <karol.piszczek@gmail.com>
2017-03-13 11:45:55 +0100

    done backup script

--- monitor/backup.py
+++ monitor/backup.py
@@ -7,7 +7,6 @@ c.execute('SELECT ip, login, password, port FROM monitoring_device')
 config = c.fetchall()
 
 host_configurations = {}
-print(config)
 
 for ip in config:
     host_configurations[ip[0]] = {'user': ip[1], 'password': ip[2], 'port': 9922}
@@ -16,10 +15,6 @@ for ip in config:
     password = "\"" + ip[2] + "\""
     port = "9922"
 
-    command = "sshpass -p %s scp -P %s -o StrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group1-sha1 root@%s:/tmp/system.cfg /home/karol/programy/monitoring/back/%s" %(password, port, ip_add, ip_add)
+    command = "sshpass -p %s scp -P %s -o ConnectTimeout=10 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group1-sha1 %s@%s:/tmp/system.cfg /home/karol/programy/monitoring/backup/%s" %(password, port, user, ip_add, ip_add)
     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
-
-#command = "sshpass -p \"Kotka09\" scp -P 9922 root@192.168.5.100:/tmp/system.cfg /home/karol/programy/monitoring/back/conf"
-
-#
