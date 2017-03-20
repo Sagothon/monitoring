59fe723b80213f464d23d230cbd5f0e0d8a4c387
Sagothon <karol.piszczek@gmail.com>
2017-03-20 23:09:46 +0100

    getting things done

--- stronka/monitoring/tables.py
+++ stronka/monitoring/tables.py
@@ -1,7 +1,14 @@
 import django_tables2 as tables
 from .models import Device
+from django.utils.safestring import mark_safe
+
+class CustomCheckBoxColumn(tables.CheckBoxColumn):
+    def render(self, value, record, bound_column):
+        box = '<input value="%s", type="checkbox", name="cBox[]"/><a href="http://%s"> %s</a>' %(value, value, value)
+        return mark_safe(box)
 
 class DevicesTable(tables.Table):
+    ip = CustomCheckBoxColumn()
     class Meta:
         model = Device
         # add class="paleblue" to <table> tag
