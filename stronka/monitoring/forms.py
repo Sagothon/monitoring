362113907c3b2850a4f8c39ae6e09fbfa1a2c24e
Sagothon <karol.piszczek@gmail.com>
2017-04-03 23:04:50 +0200

    added password form

--- stronka/monitoring/forms.py
+++ stronka/monitoring/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ConfigForm(forms.Form):
     ip = forms.CharField(max_length=100)
     login = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput)
     port = forms.CharField(max_length=100)
